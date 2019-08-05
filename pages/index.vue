5272a1624253581c446bff67187492e267451825
Myolisi Ngubentombi <myolisi@ngubentombi.co.za>
2019-08-05 12:02:21 +0200

    Initial commit

---
+++ pages/index.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <div>{{pCapitalize}} | {{pCapitalize | capitalize}}</div>
+    <div>{{pCapitalize}} | {{pCapitalize | capitalize('first')}}</div>
+    <div>{{pDecapitalize}} | {{pDecapitalize | decapitalize}}</div>
+    <div>{{pEntities}} | {{pEntities | htmlEntities}}</div>
+    <div>{{pCamelcase}} | {{pCamelcase | camelcase}}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      pCapitalize: "james may",
+      pDecapitalize: "JAMES MAY",
+      pEntities: "Hello &quot;james&quot; may",
+      pCamelcase: "astaLavistaBaby"
+    };
+  },
+  mounted() {
+    console.log(this.$options.filters.capitalize(this.name)); //JAMES MAY
+    //or
+    console.log(this.$capitalize("james"));
+  }
+};
+</script>
+<style scoped>
+</style>
---
+++ plugins/filters.js
@@ -0,0 +1,4 @@
+import Vue from "vue";
+import vueFiltersCollection from "vue-filters-collection";
+
+Vue.use(vueFiltersCollection);
